421731f85d7e2fb48a40bdb894a6b65434976069
Gracia Juniora <118818648+gjravllnv@users.noreply.github.com>
2023-05-21 19:21:00 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\CSFE2\\csfe2_p5_api" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\CSFE2\\csfe2_p5_api"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\CSFE2\\csfe2_p5_api\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\CSFE2\\csfe2_p5_api"
+  "FLUTTER_TARGET=D:\\CSFE2\\csfe2_p5_api\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\CSFE2\\csfe2_p5_api\\.dart_tool\\package_config.json"
+)
